c2f09776e1928b40bc2f2c22d9b1a9ad43926bde
Sullivan Smith <sullysmith46@gmail.com>
2025-04-07 16:59:29 -0400

    Added object to screen

--- objects/main.asm
+++ objects/main.asm
@@ -1,16 +1,37 @@
+; The code below, when compiled and run on an emulator/Game Boy will display an image of bricks
+; as well as a duck. This is used explore the basics of tile mapping and copying memory.
+
+
+; Has same effect as if file was copied above right here
+; 'hardware.inc' defines a lot of hardware memory address constants
 INCLUDE "hardware.inc"
 
+; Define the Header section
+; Section is a key word to define a section
+; "Header" is the name we have chosen to name this section (although it could be left blank)
+; ROM0 says which memory type the section belongs to
+; [$100] tells where section is in memory (in this case where in ROM0)
 SECTION "Header", ROM0[$100]
 
+	; Sets the program counter (PC) to the 'EntryPoint' address definied below
 	jp EntryPoint
-	
+
+	; ds statically allocates memory to reserve some amount of bytes
+	; $150 - @ is the number of bytes to reserve (read as: '$150 minus the current memory address')
+	; 0 is optional but says what to set the memory to	
 	ds $150 - @ ; Make room for the header
 
+; The EntryPoint to begin code execution at
 EntryPoint:
 	; Do not turn the LCD off outside of VBlank
+; An address that is used to loop until VBlank (this helps to not damage screen by writing when not supposed to)
 WaitVBlank:
+	; Put's the data in memory address rLY ($FF44) into the a register
 	ld a, [rLY]
+	; Compares the immediate value '144' with what's in register a (which we just loaded). This updates flags as necessary.
+	; This essentially is doing 'a - 144' and updating the flags with the answer.
 	cp 144
+	; Set the program counter to the WaitVBlank address if the c flag is set (meaning the last comparison resulted in a value <= 0)
 	jp c, WaitVBlank
 
 	; Turn the LCD off
@@ -18,18 +39,32 @@ WaitVBlank:
 	ld [rLCDC], a
 
 	; Copy the tile data
+	; Address to copy from
 	ld de, Tiles
+	; Address to copy to in VRAM
 	ld hl, $9000
+	; How many bytes will need to copy over
 	ld bc, TilesEnd - Tiles
 CopyTiles:
+	; Copy tile data from address on 
 	ld a, [de]
+	; Put this tile data in the address storing to in VRAM
 	ld [hli], a
+	; Increment where at in tile data
 	inc de
+	; Decrement the number of bytes remaining to copy
 	dec bc
+
+	; Copy the value from register b into register a
 	ld a, b
+	; Bit-wise or register c with register a
 	or a, c
+	; Jump if the last operation didn't overflow (a|c == 0)
+	; This means there are no more bytes to copy over from tile data
 	jp nz, CopyTiles
 
+
+	; Repeat the same thing as was for the 'CopyTiles' stuff, but for the tile map (now in VRAM @ $9800)
 	; Copy the tilemap
 	ld de, Tilemap
 	ld hl, $9800
@@ -42,17 +77,65 @@ CopyTilemap:
 	ld a, b
 	or a, c
 	jp nz, CopyTilemap
+
+
+	; Copy the paddle tile
+	ld de, Paddle
+	ld hl, $8000
+	ld bc, PaddleEnd - Paddle
+CopyPaddle:
+	ld a, [de]
+	ld [hli], a
+	inc de
+	dec bc
+	ld a, b
+	or a, c
+	jp nz, CopyPaddle
+
+	; Good to do this now since like VRAM, screen must be off to safely acces OAM
+	ld a, 0
+	ld b, 160
+	ld hl, _OAMRAM
+ClearOam:
+	ld [hli], a
+	dec b
+	jp nz, ClearOam
+	
+	
+	; Draw an object by writing its properties
+	; Each object is 4 bytes (in order): Y, X, Tile ID, Attributes
+	ld hl, _OAMRAM
+
+	; Object's top-left pixel is 128 pixels from top of screen
+	ld a, 128 + 16
+	ld [hli], a
+
+	; 16 from its left
+	ld a, 16 + 8
+	ld [hli], a
+
+	; Tile ID and attributes are set to 0
+	ld a, 0
+	ld [hli], a
+	ld [hli], a
+
+
 	
 	; Turn the LCD on
-	ld a, LCDCF_ON | LCDCF_BGON
+	ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON
 	ld [rLCDC], a
 
 	; During the first (blank) frame, initialize display registers
 	ld a, %11100100
 	ld [rBGP], a
+	ld a, %11100100
+	ld [rOBP0], a
 
+; Infinite loop so doesn't execute non-executable memory
 Done:
 	jp Done
+
+; Below are the sections for the tile data, and the tile map data
 
 Section "Tile data", ROM0
 
@@ -291,3 +374,14 @@ Tilemap:
 	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $16, $17, $18, $19, $03, 0,0,0,0,0,0,0,0,0,0,0,0
 	db $04, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
 TilemapEnd:
+
+Paddle:
+    dw `13333331
+    dw `30000003
+    dw `13333331
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+PaddleEnd:
