2f7ee2d01502c3b72e2aeb1a436cad79c4d41127
Sullivan Smith <sullysmith46@gmail.com>
2025-04-08 20:10:25 -0400

    Added memcopy function

---
+++ function/main.asm
@@ -0,0 +1,408 @@
+; The code below, when compiled and run on an emulator/Game Boy will display an image of bricks
+; as well as a duck. This is used explore the basics of tile mapping and copying memory.
+
+
+; Has same effect as if file was copied above right here
+; 'hardware.inc' defines a lot of hardware memory address constants
+INCLUDE "hardware.inc"
+
+; Define the Header section
+; Section is a key word to define a section
+; "Header" is the name we have chosen to name this section (although it could be left blank)
+; ROM0 says which memory type the section belongs to
+; [$100] tells where section is in memory (in this case where in ROM0)
+SECTION "Header", ROM0[$100]
+
+	; Sets the program counter (PC) to the 'EntryPoint' address definied below
+	jp EntryPoint
+
+	; ds statically allocates memory to reserve some amount of bytes
+	; $150 - @ is the number of bytes to reserve (read as: '$150 minus the current memory address')
+	; 0 is optional but says what to set the memory to	
+	ds $150 - @ ; Make room for the header
+
+
+; Copy bytes from one area to another.
+; @param de: Source
+; @param hl: Destination
+; @param bc: Length
+Memcopy:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, Memcopy
+    ret
+
+; The EntryPoint to begin code execution at
+EntryPoint:
+	; Do not turn the LCD off outside of VBlank
+; An address that is used to loop until VBlank (this helps to not damage screen by writing when not supposed to)
+WaitVBlank:
+	; Put's the data in memory address rLY ($FF44) into the a register
+	ld a, [rLY]
+	; Compares the immediate value '144' with what's in register a (which we just loaded). This updates flags as necessary.
+	; This essentially is doing 'a - 144' and updating the flags with the answer.
+	cp 144
+	; Set the program counter to the WaitVBlank address if the c flag is set (meaning the last comparison resulted in a value <= 0)
+	jp c, WaitVBlank
+
+	; Turn the LCD off
+	ld a, 0
+	ld [rLCDC], a
+
+	; Copy the tile data
+	; Address to copy from
+	ld de, Tiles
+	; Address to copy to in VRAM
+	ld hl, $9000
+	; How many bytes will need to copy over
+	ld bc, TilesEnd - Tiles
+	call Memcopy
+
+	; Repeat the same thing as was for the 'CopyTiles' stuff, but for the tile map (now in VRAM @ $9800)
+	; Copy the tilemap
+	ld de, Tilemap
+	ld hl, $9800
+	ld bc, TilemapEnd - Tilemap
+	call Memcopy
+
+	; Copy the paddle tile
+	ld de, Paddle
+	ld hl, $8000
+	ld bc, PaddleEnd - Paddle
+	call Memcopy
+
+	; Good to do this now since like VRAM, screen must be off to safely acces OAM
+	ld a, 0
+	ld b, 160
+	ld hl, _OAMRAM
+ClearOam:
+	ld [hli], a
+	dec b
+	jp nz, ClearOam
+	
+	
+	; Draw an object by writing its properties
+	; Each object is 4 bytes (in order): Y, X, Tile ID, Attributes
+	ld hl, _OAMRAM
+
+	; Object's top-left pixel is 128 pixels from top of screen
+	ld a, 128 + 16
+	ld [hli], a
+
+	; 16 from its left
+	ld a, 16 + 8
+	ld [hli], a
+
+	; Tile ID and attributes are set to 0
+	ld a, 0
+	ld [hli], a
+	ld [hli], a
+
+
+	
+	; Turn the LCD on
+	ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON
+	ld [rLCDC], a
+
+	; During the first (blank) frame, initialize display registers
+	ld a, %11100100
+	ld [rBGP], a
+	ld a, %11100100
+	ld [rOBP0], a
+
+
+	; Initialize global variables
+	ld a, 0
+	ld [wFrameCounter], a
+
+; Infinite loop so doesn't execute non-executable memory
+; This controls the movement of the paddle object on the screen
+Main:
+	; Wait until it's *not* VBlank
+	ld a, [rLY]
+	cp 144
+	jp nc, Main
+WaitVBlank2:
+	; Wait until it *is* VBlank
+	ld a, [rLY]
+	cp 144
+	jp c, WaitVBlank2
+
+	ld a, [wFrameCounter]
+	inc a
+	ld [wFrameCounter], a
+	cp a, 15 ; Every 15 frames (a quarter second), run the following code
+	jp nz, Main
+
+	; Reset the frame counter back to 0
+	ld a, 0
+	ld [wFrameCounter], a
+	
+	; Move the paddle one pixel to the right
+	; Put X Pos in register a
+	ld a, [_OAMRAM + 1]
+	; Increment the X Pos
+	inc a
+	; Put the X Pos back in memory
+	ld [_OAMRAM + 1], a
+	; loop
+	jp Main
+
+; Below are the sections for the tile data, and the tile map data
+
+Section "Tile data", ROM0
+
+Tiles:
+	dw `33333333
+	dw `33333333
+	dw `33333333
+	dw `33322222
+	dw `33322222
+	dw `33322222
+	dw `33322211
+	dw `33322211
+	dw `33333333
+	dw `33333333
+	dw `33333333
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `11111111
+	dw `11111111
+	dw `33333333
+	dw `33333333
+	dw `33333333
+	dw `22222333
+	dw `22222333
+	dw `22222333
+	dw `11222333
+	dw `11222333
+	dw `33333333
+	dw `33333333
+	dw `33333333
+	dw `33333333
+	dw `33333333
+	dw `33333333
+	dw `33333333
+	dw `33333333
+	dw `33322211
+	dw `33322211
+	dw `33322211
+	dw `33322211
+	dw `33322211
+	dw `33322211
+	dw `33322211
+	dw `33322211
+	dw `22222222
+	dw `20000000
+	dw `20111111
+	dw `20111111
+	dw `20111111
+	dw `20111111
+	dw `22222222
+	dw `33333333
+	dw `22222223
+	dw `00000023
+	dw `11111123
+	dw `11111123
+	dw `11111123
+	dw `11111123
+	dw `22222223
+	dw `33333333
+	dw `11222333
+	dw `11222333
+	dw `11222333
+	dw `11222333
+	dw `11222333
+	dw `11222333
+	dw `11222333
+	dw `11222333
+	dw `00000000
+	dw `00000000
+	dw `00000000
+	dw `00000000
+	dw `00000000
+	dw `00000000
+	dw `00000000
+	dw `00000000
+	dw `11001100
+	dw `11111111
+	dw `11111111
+	dw `21212121
+	dw `22222222
+	dw `22322232
+	dw `23232323
+	dw `33333333
+	; Paste your logo here:
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222211
+	dw `22222211
+	dw `22222211
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `11111111
+	dw `11111111
+	dw `11221111
+	dw `11221111
+	dw `11000011
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `11222222
+	dw `11222222
+	dw `11222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222211
+	dw `22222200
+	dw `22222200
+	dw `22000000
+	dw `22000000
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `11000011
+	dw `11111111
+	dw `11111111
+	dw `11111111
+	dw `11111111
+	dw `11111111
+	dw `11111111
+	dw `11000022
+	dw `11222222
+	dw `11222222
+	dw `11222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222200
+	dw `22222200
+	dw `22222211
+	dw `22222211
+	dw `22221111
+	dw `22221111
+	dw `22221111
+	dw `11000022
+	dw `00112222
+	dw `00112222
+	dw `11112200
+	dw `11112200
+	dw `11220000
+	dw `11220000
+	dw `11220000
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22000000
+	dw `22000000
+	dw `00000000
+	dw `00000000
+	dw `00000000
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `22222222
+	dw `11110022
+	dw `11110022
+	dw `11110022
+	dw `22221111
+	dw `22221111
+	dw `22221111
+	dw `22221111
+	dw `22221111
+	dw `22222211
+	dw `22222211
+	dw `22222222
+	dw `11220000
+	dw `11110000
+	dw `11110000
+	dw `11111111
+	dw `11111111
+	dw `11111111
+	dw `11111111
+	dw `22222222
+	dw `00000000
+	dw `00111111
+	dw `00111111
+	dw `11111111
+	dw `11111111
+	dw `11111111
+	dw `11111111
+	dw `22222222
+	dw `11110022
+	dw `11000022
+	dw `11000022
+	dw `00002222
+	dw `00002222
+	dw `00222222
+	dw `00222222
+	dw `22222222
+
+TilesEnd: 
+
+Section "Tilemap", ROM0
+
+Tilemap:
+	db $00, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $02, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $0A, $0B, $0C, $0D, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $0E, $0F, $10, $11, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $12, $13, $14, $15, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $16, $17, $18, $19, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+TilemapEnd:
+
+Paddle:
+    dw `13333331
+    dw `30000003
+    dw `13333331
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+PaddleEnd:
+
+; Global variable for keeping track of when last moved paddle
+SECTION "Counter", WRAM0
+wFrameCounter: db
